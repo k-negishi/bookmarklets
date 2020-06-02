507acee3f2a31a86bd259ff8f97449e41a869f97
Kei Negishi <56011598+keinegishi@users.noreply.github.com>
2020-06-03 03:01:53 +0900

    add semicolon

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 javascript:(function () {
-  const amazonUrl = "https://www.amazon.co.jp/s"
+  const amazonUrl = "https://www.amazon.co.jp/s";
   const url = location.href;
-  const query = '&emi=AN1VRQENFRJN5'
+  const query = '&emi=AN1VRQENFRJN5';
 
   if (url.split('?')[0] === amazonUrl) {
     window.open(url + query);
   } else {
     alert('エラー\namazon.co.jp の検索結果画面でご使用ください');
   }
-})()
+})();
